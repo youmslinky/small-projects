c49db4f020474fe7534200b4238a687f6c4379b7
youmslinky <youmslinky1@gmail.com>
2020-03-29 18:36:12 -0500

    Add comments and ability to switch on race demonstration

--- parallel/race.cc
+++ parallel/race.cc
@@ -3,6 +3,14 @@
 #include <unistd.h>
 #include <mutex>
 
+// #define SHARED
+//this demonstrates a race condition with printing using std::cout
+
+//if SHARED is defined, it will use a mutex regulated print
+
+//if SHARED is not defined, when running the program, there will be random
+//cases where cout didn't finish from main, or from the thread 
+//basically, the main program and thread will stomp on each other's print statements
 
 using namespace std;
 
@@ -10,30 +18,39 @@ mutex mu;
 
 void shared_print(string msg, int id)
 {
-	mu.lock();
-	cout << msg << id << endl;
-	mu.unlock();
+    mu.lock();
+    cout << msg << id << endl;
+    mu.unlock();
+}
+
+void non_shared_print(string msg, int id)
+{
+    cout << msg << id << endl;
 }
 
 void a_function()
 {
-	for(int i=0;i>-200;i--)
-		shared_print("From t1: ",i);
-	int blah;
-	cin >> blah; 
+    for(int i=0;i>-200;i--)
+#ifdef SHARED
+        shared_print("From t1: ",i);
+#endif
+#ifndef SHARED
+        non_shared_print("From t1: ",i);
+#endif
 }
 
 int main(){
-	thread t1(a_function);
+    thread t1(a_function);
+    // t1.detach();
 
-	for(int i=0;i<200;i++)
-		shared_print("From main: ",i);
+    for(int i=0;i<200;i++)
+#ifdef SHARED
+        shared_print("From Main: ",i);
+#endif
+#ifndef SHARED
+        non_shared_print("From Main: ",i);
+#endif
 
-	//t1.detach();
-	t1.join();
-	cout << "joined with thread" << endl;
-	int blah;
-	cin >> blah; 
-	cout << "end of program here" << endl;
-	return 0;
+    t1.join();
+    return 0;
 }
